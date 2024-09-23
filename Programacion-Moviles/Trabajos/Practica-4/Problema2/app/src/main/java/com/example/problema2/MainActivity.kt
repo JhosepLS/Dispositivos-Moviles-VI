ac11bdfa9e9fd68a9154d57a09d4cae99745425a
JhosepLS <jmollapazam@ulasalle.edu.pe>
2024-09-23 01:34:53 -0500

    Creando el archivo de la actividad principal

--- Programacion-Moviles/Trabajos/Practica-4/Problema2/app/src/main/java/com/example/problema2/MainActivity.kt
+++ Programacion-Moviles/Trabajos/Practica-4/Problema2/app/src/main/java/com/example/problema2/MainActivity.kt
@@ -1,47 +1,33 @@
-package com.example.problema2
+/*
+ * Descripción: Actividad principal que actúa como contenedor para los fragmentos de selección y reproducción de música
+ * Autor: Jhosep
+ * Fecha creación: 23 de septiembre del 2024
+ * Fecha última modificación: 23 de septiembre del 2024
+ */
 
+package com.example.practica3
+
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.problema2.ui.theme.Problema2Theme
+import androidx.fragment.app.commit
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), AudioSelectionListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Problema2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                add(R.id.fragment_container, SeleccionAudioFragment())
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Problema2Theme {
-        Greeting("Android")
+    override fun onAudioSelected(position: Int, nombre: String) {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, ReproductorFragment.newInstance(position, nombre))
+            addToBackStack(null)
+        }
     }
 }

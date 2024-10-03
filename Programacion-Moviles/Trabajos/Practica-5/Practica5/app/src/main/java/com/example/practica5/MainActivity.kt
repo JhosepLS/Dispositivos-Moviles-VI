45b737c9b4f5f2da48514cd9e0085869768a40ca
JhosepLS <jmollapazam@ulasalle.edu.pe>
2024-10-03 00:37:22 -0500

    Modificamos el MainActivity

--- Programacion-Moviles/Trabajos/Practica-5/Practica5/app/src/main/java/com/example/practica5/MainActivity.kt
+++ Programacion-Moviles/Trabajos/Practica-5/Practica5/app/src/main/java/com/example/practica5/MainActivity.kt
@@ -1,47 +1,34 @@
+/*
+ * Descripción: Actividad principal que maneja la navegación entre fragmentos
+ * Autor: Assistant
+ * Fecha creación: 3 de octubre del 2024
+ * Fecha última modificación: 3 de octubre del 2024
+ */
+
 package com.example.practica5
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.practica5.ui.theme.Practica5Theme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), AudioSelectionListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Practica5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SeleccionAudioFragment())
+                .commit()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Practica5Theme {
-        Greeting("Android")
+    override fun onAudioSelected(audio: Audio) {
+        val fragment = ReproductorFragment.newInstance(audio)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
